418e915ac2d503143d011818ababd8aff9f76275
khalid <khalid@eigerlabtech.com>
2025-07-11 14:00:21 +0530

    Initial Commit

---
+++ BQE/BQEApp.swift
@@ -0,0 +1,17 @@
+//
+//  BQEApp.swift
+//  BQE
+//
+//  Created by Khalid on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BQEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
